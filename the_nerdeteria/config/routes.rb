820be8323d2f223aca632e4c080f2df05c5f5ec8
Damion M Broadaway <dbroadaw@nerdery.com>
2014-05-09 15:11:14 -0500

    added three models and some css

--- the_nerdeteria/config/routes.rb
+++ the_nerdeteria/config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  resources :menu_item_catergories
+
+  resources :restaurants
+
   resources :menu_items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'menu_items#index'
+
+  get '/info' => 'menu_items#info'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
